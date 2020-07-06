73b274d5ba5fb35c70428b48ec1e805520be27c8
Dihfahsih1 <dihfahsihm@gmail.com>
2020-07-06 18:29:09 +0300

    more changes on the features

--- datamasterapp/urls.py
+++ datamasterapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^online-estimates-and-quotations', views.estimates_quotations, name='estimates-quotations'),
     url(r'^online-invoicing-and-billing', views.invoicing_billing, name='invoicing-billing'),
     url(r'^expenses-management', views.expenses_purchases, name='expenses-management'),
+    url(r'^simple-online-accounting', views.accounting_reporting, name='accounting-reporting'),
     
 ]
