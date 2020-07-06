e86c746060248462d991de5c7fe7b26371818d1c
Dihfahsih1 <dihfahsihm@gmail.com>
2020-07-06 16:51:42 +0300

    things are not direct as they seem

--- datamasterapp/urls.py
+++ datamasterapp/urls.py
@@ -5,4 +5,6 @@ from datamasterapp import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^online-estimates-and-quotations', views.estimates_quotations, name='estimates-quotations'),
+    url(r'^online-invoicing-and-billing', views.invoicing_billing, name='invoicing-billing'),
+    
 ]
