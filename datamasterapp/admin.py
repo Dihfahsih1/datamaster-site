de06e9393da503c1e9999586576530da6031f7b7
Dihfahsih1 <dihfahsihm@gmail.com>
2020-07-11 11:06:10 +0300

    create the slider on the datamaster

--- datamasterapp/admin.py
+++ datamasterapp/admin.py
@@ -6,5 +6,6 @@ admin.site.register(Reason)
 admin.site.register(FeatureDetail)
 admin.site.register(Feedback)
 admin.site.register(CompanyDetail)
+admin.site.register(Slider)
 
 # Register your models here.
--- datamasterapp/views.py
+++ datamasterapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from.models import *
 from.forms import *
 from django.core.mail import EmailMessage
+
 def index(request):
 	features = Feature.objects.all()
 	reasons = Reason.objects.all()
